15d6466f15b5fc612b37e652ac6f3f55e96371b0
Marcos Schead <wssmarcos@gmail.com>
2023-07-20 00:21:41 -0300

    [Web] Added NextJS project

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="prose mx-auto text-center mt-10">
+      <h3>Please connect your Metamask Wallet.</h3>
+    </div>
+  );
+}
---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,21 @@
+import { PropsWithChildren } from "react";
+
+export default function Layout({ children }: PropsWithChildren) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="bg-gray-800 mb-8 py-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <a>
+            <span className="mx-auto text-cyan-400">Decentralized Blog</span>
+          </a>
+        </div>
+      </header>
+      <main className="container mx-auto flex-1">{children}</main>
+      <footer className="bg-gray-800 mt-8 py-4">
+        <div className="container mx-auto flex justify-center text-cyan-400">
+          &copy; Marcos Schead - 2023
+        </div>
+      </footer>
+    </div>
+  );
+}
